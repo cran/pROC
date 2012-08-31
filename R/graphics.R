c8a2d073eab18b6b03a5eb031edd3c2955d91901
Xavier Robin <Xavier.Robin@unige.ch>
2012-08-31 00:00:00 +0000

    version 1.5.4

---
+++ R/graphics.R
@@ -0,0 +1,23 @@
+# pROC: Tools Receiver operating characteristic (ROC curves) with
+# (partial) area under the curve, confidence intervals and comparison. 
+# Copyright (C) 2012 Xavier Robin, Alexandre Hainard, Natacha Turck,
+# Natalia Tiberti, Frédérique Lisacek, Jean-Charles Sanchez
+# and Markus Müller
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+# Remove the "missing arguments" warnings from graphical functions.
+# Inspired from Gavin Simpson
+# https://ucfagls.wordpress.com/2011/07/23/passing-non-graphical-parameters-to-graphical-functions-using/
